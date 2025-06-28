0bf8124ab9e508360b216301b339126c8fc765f4
Candice Ho <Candy75@users.noreply.github.com>
2025-06-29 00:30:06 +0800

    Revise for adding health check

--- main.py
+++ main.py
@@ -307,3 +307,7 @@ if __name__ == "__main__":
    
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port, log_level="info", )
+
+@app.get("/health")
+def health():
+    return {"status":"ok"}
