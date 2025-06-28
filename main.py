3561e16928e28b875ddd3613a6feba3a0b73ceeb
Candice Ho <Candy75@users.noreply.github.com>
2025-06-29 00:23:40 +0800

    Revise for showing log level

--- main.py
+++ main.py
@@ -306,4 +306,4 @@ if __name__ == "__main__":
     print("=" * 50)
    
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info", )
